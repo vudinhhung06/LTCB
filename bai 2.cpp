1acdc3628a314ca03b59da47a45b21aa2e37af5b
Vũ Đình Hưng - AT210523 <vudinhhung10022006@gmail.com>
2025-03-25 08:06:18 +0700

    Add files via upload

---
+++ bai 2.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	int ngay, thang, nam;
+	printf("hay nhap ngay, thangn, nam :");
+	scanf(" %d %d %d",&ngay, &thang, &nam);
+	printf("hom nay ngay: %d/%d/%d ", ngay, thang, nam);
+	return 0;
+}
